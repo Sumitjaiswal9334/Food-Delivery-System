5487fd0fc73a68867224eddec92e736f1eb31f4f
SUMIT KUMAR JAISWAL <166597425+Sumitjaiswal9334@users.noreply.github.com>
2025-01-28 00:41:23 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -9,7 +9,7 @@ const StoreContextProvider = (props) => {
 
 
     const [cartItems, setCartItems] = useState({});
-    const url = "http://localhost:4000";
+    const url = "https://tomato-backend-z2rb.onrender.com";
     const [token,setToken] = useState("")
     const [food_list,setFoodList] = useState([])
 
